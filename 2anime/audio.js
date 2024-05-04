b469d77637eaea2da3c56cfa944442fc7c601e5d
r0o0di <99562096+r0o0di@users.noreply.github.com>
2024-05-05 00:46:48 +0200

    Add files via upload

--- 2anime/audio.js
+++ 2anime/audio.js
@@ -458,14 +458,9 @@ function handleRowClick(event) {
                     if (deltaX > 0) {
                         // Swiped right (previous song)
                         playPreviousSong();
-                        titleAlbumContainer.classList.add('swipe-right');
-                        titleAlbumContainer.classList.remove('swipe-left');
-
                     } else {
                         // Swiped left (next song)
                         playNextSong();
-                        titleAlbumContainer.classList.add('swipe-left');
-                        titleAlbumContainer.classList.remove('swipe-right');
                     }
 
                     // Reset startX and startY to prevent continuous swiping
@@ -478,10 +473,6 @@ function handleRowClick(event) {
         function handleEnd() {
             startX = null;
             startY = null;
-
-            // Reset swipe classes
-            titleAlbumContainer.classList.remove('swipe-left');
-            titleAlbumContainer.classList.remove('swipe-right');
         }
 
         // Function to play the previous song
