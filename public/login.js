9f37a07e5fae893e4d7fbdf1e4c39324fe6ac1ff
r0o0di <99562096+r0o0di@users.noreply.github.com>
2024-07-05 01:45:02 +0200

    Add files via upload

--- public/login.js
+++ public/login.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
 // import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
-import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
 // import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 const firebaseConfig = {
     apiKey: "AIzaSyC-CSH_18h-ju2i71IJi0jUvvrtqoqY0hs",
     authDomain: "detective-conan-tracklist.firebaseapp.com",
@@ -23,49 +20,29 @@ const provider = new GoogleAuthProvider();
 // const analytics = getAnalytics(app);
 // const firestore = getFirestore(app);
 
-// submit
-const submit = document.getElementById("submit");
-submit.addEventListener("click", function (event) {
-    event.preventDefault(); // prevenets the default behaviour of refreshing the page when clicked.
-
-    // inputs
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed up 
-            const user = userCredential.user;
-            alert("Account Created");
-            window.location.href = "../index.html";
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            alert(errorMessage);
-            // ..
-        });
-})
 
 
+const googleBtn = document.getElementById("google");
 
-// socials 
-const google = document.getElementById("google");
-google.addEventListener("click", () => {
+googleBtn.addEventListener("click", () => {
     signInWithPopup(auth, provider)
         .then((result) => {
             // This gives you a Google Access Token. You can use it to access the Google API.
             const credential = GoogleAuthProvider.credentialFromResult(result);
             const token = credential.accessToken;
-            // The signed-in user info.
             const user = result.user;
-            // IdP data available using getAdditionalUserInfo(result)
-            alert("signed in with gooogle")
-            window.location.href = "../index.html";
 
+            console.log(user.reloadUserInfo)
+            console.log(user.reloadUserInfo.photoUrl)
+            // IdP data available using getAdditionalUserInfo(result)
+            // window.location.href = "../index.html";
+            if (user) {
+                const profilePic = document.getElementById("profilePic");
+                profilePic.src = user.reloadUserInfo.photoUrl;
+                // Show other user-specific content here
+              }
             // ...
         }).catch((error) => {
-            // Handle Errors here.
             const errorCode = error.code;
             const errorMessage = error.message;
             // The email of the user's account used.
@@ -77,3 +54,28 @@ google.addEventListener("click", () => {
         });
 })
 
+
+
+const logOutBtn = document.getElementById("logOut");
+logOutBtn.addEventListener("click", () => {
+    signOut(auth).then(() => {
+        console.log("signed out");
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        alert(errorMessage);
+    });
+});
+
+
+
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const profilePic = document.getElementById("profilePic");
+    profilePic.src = user.reloadUserInfo.photoUrl;
+  } else {
+    profilePic.src = ""; 
+  }
+});
