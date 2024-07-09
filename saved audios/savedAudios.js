b8851d000ff0a9268caf14e805ceca3faa791142
r0o0di <99562096+r0o0di@users.noreply.github.com>
2024-07-09 16:23:07 +0200

    Delete saved audios directory

--- saved audios/savedAudios.js
+++
@@ -1,9 +0,0 @@
-import FromDatabase from "../public/login.js";
-FromDatabase.displayPlaylist();
-FromDatabase.displayProfilePic();
-
-import { navigation } from "../saved audios/utils.js";
-
-
-const navContainer = document.querySelector(".container");
-navigation(navContainer, "home", "heart-active");
