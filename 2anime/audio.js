a437e2d88cdf42fe111d95e297a4ebf1758ff214
r0o0di <99562096+r0o0di@users.noreply.github.com>
2024-07-20 19:17:04 +0200

    Update audio.js

--- 2anime/audio.js
+++ 2anime/audio.js
@@ -482,16 +482,6 @@ function mediaMetadata(audioElement, album, unchangedAlbum, unchangedTitle, play
 
     });
 
-    navigator.mediaSession.setActionHandler("seekbackward", (evt) => {
-        const skipTime = evt.seekOffset || 10; // Skip 10 secs
-        audioElement.currentTime = audioElement.currentTime - skipTime;
-    });
-
-    navigator.mediaSession.setActionHandler("seekforward", (evt) => {
-        const skipTime = evt.seekOffset || 10; // Skip 10 secs
-        audioElement.currentTime = audioElement.currentTime + skipTime;
-    });
-
     navigator.mediaSession.setActionHandler("seekto", (evt) => {
         const seekTime = evt.seekTime;
         audioElement.currentTime = seekTime;
