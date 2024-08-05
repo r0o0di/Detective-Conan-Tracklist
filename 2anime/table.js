2248b8bdbfa1167d6cb48b078dd21437d686daea
r0o0di <99562096+r0o0di@users.noreply.github.com>
2024-08-05 19:40:58 +0200

    Add files via upload

--- 2anime/table.js
+++ 2anime/table.js
@@ -1,32 +1,5 @@
 import allBgmData from './0data/dc-all-bgm-data.js';
 
-// function getPageNumber(episodeId) {
-//     if (episodeId >= 1 && episodeId < 100) {
-//         return 1;
-//     } else if (episodeId >= 100 && episodeId < 200) {
-//         return 2;
-//     } else if (episodeId >= 200 && episodeId < 300) {
-//         return 3;
-//     } else if (episodeId >= 300 && episodeId < 400) {
-//         return 4;
-//     } else if (episodeId >= 400 && episodeId < 500) {
-//         return 5;
-//     } else if (episodeId >= 500 && episodeId < 600) {
-//         return 6;
-//     } else if (episodeId >= 600 && episodeId < 700) {
-//         return 7;
-//     } else if (episodeId >= 700 && episodeId < 800) {
-//         return 8;
-//     } else if (episodeId >= 800 && episodeId < 900) {
-//         return 9;
-//     } else if (episodeId >= 900 && episodeId < 1000) {
-//         return 10;
-//     } else if (episodeId >= 1000 && episodeId < 1100) {
-//         return 11;
-//     } else if (episodeId >= 1100 && episodeId < 1200) {
-//         return 12;
-//     }
-// }
 function getPageNumber(episodeId) {
     return Math.floor(episodeId / 100) + 1;
 }
@@ -40,7 +13,7 @@ function generateEpisodeTable(episodeNumber) {
         table.dataset.id = episode.id; // Add data-id attribute
         table.classList.add("lazy-load");
 
-        const firstTables = [1, 2, 100, 101, 200, 201, 300, 301, 400, 401, 500, 501, 
+        const firstTables = [1, 2, 100, 101, 200, 201, 300, 301, 400, 401, 500, 501,
             600, 601, 700, 701, 800, 801, 900, 901, 1000, 1001, 1100, 1101, 1200, 1201,
             1300, 1301, 1400, 1401, 1500, 1501];
         if (firstTables.includes(Number(table.id))) {
@@ -83,10 +56,12 @@ function generateEpisodeTable(episodeNumber) {
         table.appendChild(caption);
 
         const pageNumber = getPageNumber(Number(episode.id));
-        const tableContainer = document.getElementById(`page${pageNumber}`);
-        if (tableContainer) {
-            tableContainer.querySelector('.table-container').appendChild(table);
-        }
+        const page = document.getElementById(`page${pageNumber}`);
+        const tableContainer = document.createElement("div");
+        tableContainer.classList.add("table-container");
+        tableContainer.appendChild(table);
+        page?.querySelector('.tables-container').appendChild(tableContainer);
+
     }
 }
 
