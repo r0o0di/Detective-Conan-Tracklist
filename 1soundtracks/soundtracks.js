d2df1ee0814ffb3805ed0de37df9d53c3f062ae2
r0o0di <99562096+r0o0di@users.noreply.github.com>
2024-08-10 23:46:28 +0200

    Add files via upload

--- 1soundtracks/soundtracks.js
+++ 1soundtracks/soundtracks.js
@@ -90,8 +90,8 @@ function getFileLink(album) {
         .replace("Detective Conan Original Soundtrack 2", "https://www.mediafire.com/file/6svgsbmi9zayir9/TVOST2.zip")
         .replace("Detective Conan Original Soundtrack 3", "https://www.mediafire.com/file/qoj1kruro7khjet/TVOST3.zip")
         .replace("Detective Conan Original Soundtrack 4 Isoge Shōnen Tanteidan", "https://www.mediafire.com/file/i4p58grpuwpm46e/TVOST4.zip")
-        .replace("Detective Conan Original Soundtrack Super Best", "https://www.mediafire.com/file/xnippcserpbhvhy/TVOSTSuperBest.zip")
         .replace("Detective Conan Original Soundtrack Super Best 2", "https://www.mediafire.com/file/ajnl9yy7tabgg6w/TVOSTSuperBest2.zip")
+        .replace("Detective Conan Original Soundtrack Super Best", "https://www.mediafire.com/file/xnippcserpbhvhy/TVOSTSuperBest.zip")
         .replace("Detective Conan TV Original Soundtrack Selection Best", "https://www.mediafire.com/file/oayifdqd53u5zf1/TVOSTSelectionBest.zip")
     
         /*movie OSTs*/
@@ -100,6 +100,10 @@ function getFileLink(album) {
         .replace("Detective Conan 'The Last Wizard of the Century' Original Soundtrack", "https://www.mediafire.com/file/nwttn5dkkfs4ouo/Movie3OST.zip")
         .replace("Detective Conan 'Captured in Her Eyes' Original Soundtrack", "https://www.mediafire.com/file/9n2z6df6qthfe4p/Movie4OST.zip")
         .replace("Detective Conan 'Countdown to Heaven' Original Soundtrack", "https://www.mediafire.com/file/dwuj1w2sk87gxyu/Movie5OST.zip")
-        .replace("Detective Conan 'The Phantom of Baker Street' Original Soundtrack", "https://www.mediafire.com/file/nvtmttg0ot8p3af/Movie6OST.zip");
+        .replace("Detective Conan 'The Phantom of Baker Street' Original Soundtrack", "https://www.mediafire.com/file/nvtmttg0ot8p3af/Movie6OST.zip")
+        
+        
+        .replace("Boku ga Iru TV Anime Detective Conan Image Song Album", "https://www.mediafire.com/file/xxiy1jenmffyapf/Boku_ga_Iru_%257E_TV_Anime_%2527Detective_Conan%2527_Image_Song_Album.zip")
+        .replace("Detective Conan Character Song Collection Teitan Shougakkou ni Zenin Shuugou", "https://www.mediafire.com/file/34lrd5tswo2e19y/Detective_Conan_Character_Song_Collection_-_Teitan_Shougakkou_ni_Zenin_Shuugou%2521%2521.zip");
         return album;
 }
